ea50e4b1c4bf17d3e0b6ff2b707027027a993c60
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-01-05 21:00:38 +0000

    DRC-2096 converted everything except the logger to use packet communicators. fixbamboo codeFreeze

--- src/it/iit/iCub/sensors/IcubSensorSuiteManager.java
+++ src/it/iit/iCub/sensors/IcubSensorSuiteManager.java
@@ -4,8 +4,7 @@ import java.net.URI;
 
 import us.ihmc.SdfLoader.SDFFullRobotModel;
 import us.ihmc.communication.AbstractNetworkProcessorNetworkingManager;
-import us.ihmc.communication.net.LocalObjectCommunicator;
-import us.ihmc.communication.net.ObjectCommunicator;
+import us.ihmc.communication.net.PacketCommunicator;
 import us.ihmc.communication.producers.RobotPoseBuffer;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.camera.SCSCameraDataReceiver;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData.SCSPointCloudDataReceiver;
@@ -25,7 +24,7 @@ public class IcubSensorSuiteManager implements DRCSensorSuiteManager
       this.sensorInformation = sensorInformation;
    }
 
-   public void initializeSimulatedSensors(LocalObjectCommunicator scsCommunicator, ObjectCommunicator fieldCommunicator, RobotPoseBuffer robotPoseBuffer,
+   public void initializeSimulatedSensors(PacketCommunicator scsCommunicator, PacketCommunicator fieldCommunicator, RobotPoseBuffer robotPoseBuffer,
                                           AbstractNetworkProcessorNetworkingManager networkingManager, SDFFullRobotModel sdfFullRobotModel, DepthDataFilter lidarDataFilter, URI sensorURI)
    {
       new SCSCameraDataReceiver(robotPoseBuffer, scsCommunicator, networkingManager, ppsTimestampOffsetProvider);
@@ -36,7 +35,7 @@ public class IcubSensorSuiteManager implements DRCSensorSuiteManager
    }
 
    public void initializePhysicalSensors(RobotPoseBuffer robotPoseBuffer, AbstractNetworkProcessorNetworkingManager networkingManager,
-                                         SDFFullRobotModel sdfFullRobotModel, ObjectCommunicator objectCommunicator, DepthDataFilter lidarDataFilter, URI sensorURI)
+                                         SDFFullRobotModel sdfFullRobotModel, PacketCommunicator objectCommunicator, DepthDataFilter lidarDataFilter, URI sensorURI)
    {
    }
 
