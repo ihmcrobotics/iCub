bb571a97b41578279086f782fa426efef6639b4a
Nathan Mertins <nmertins@ihmc.us>
2015-03-24 19:49:52 +0000

    NONE: added unimplemented methods added to DRCJointMap; fixbamboo

--- src/it/iit/iCub/parameters/IcubJointMap.java
+++ src/it/iit/iCub/parameters/IcubJointMap.java
@@ -60,6 +60,8 @@ import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 import us.ihmc.utilities.robotSide.RobotSide;
 import us.ihmc.utilities.robotSide.SideDependentList;
 import us.ihmc.wholeBodyController.DRCRobotJointMap;
+import us.ihmc.yoUtilities.controllers.YoPDGains;
+import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
 
 public class IcubJointMap implements DRCRobotJointMap 
 {
@@ -360,6 +362,12 @@ public class IcubJointMap implements DRCRobotJointMap
 
    @Override
    public String[] getPositionControlledJointsForSimulation()
+   {
+      return null;
+   }
+
+   @Override
+   public List<Pair<String, YoPDGains>> getPassiveJointNameWithGains(YoVariableRegistry registry)
    {
       return null;
    }
