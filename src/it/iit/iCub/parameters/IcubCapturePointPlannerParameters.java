e1ee30d652492873d19db3c22c5ec445c0add969
Sylvain Bertrand <sbertrand@ihmc.us>
2015-04-08 21:21:11 +0000

    NONE: Extended CapturePointPlannerParameters to include the parameters needed for the new ICP trajectory. Also renamed the methods getCapturePointInFromFootCenterDistance() and getCapturePointForwardFromFootCenterDistance() to getReferenceCMPInsideOffset() and getReferenceCMPForwardOffset(). Won't fixbamboo.

--- src/it/iit/iCub/parameters/IcubCapturePointPlannerParameters.java
+++ src/it/iit/iCub/parameters/IcubCapturePointPlannerParameters.java
@@ -4,24 +4,24 @@ import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerPar
 
 public class IcubCapturePointPlannerParameters implements CapturePointPlannerParameters
 {
-	private boolean runningOnRealRobot;
+   private boolean runningOnRealRobot;
 
-	public IcubCapturePointPlannerParameters(boolean runningOnRealRobot)
-	{
-		this.runningOnRealRobot = runningOnRealRobot;
-	}
-	
-	@Override
-	public double getDoubleSupportInitialTransferDuration()
-	{
-		return runningOnRealRobot ? 2.0 : 1.0; 
-	}
+   public IcubCapturePointPlannerParameters(boolean runningOnRealRobot)
+   {
+      this.runningOnRealRobot = runningOnRealRobot;
+   }
 
-	@Override
-	public double getDoubleSupportDuration()
-	{
-		return runningOnRealRobot ? 1.5 : 0.25;
-	}
+   @Override
+   public double getDoubleSupportInitialTransferDuration()
+   {
+      return runningOnRealRobot ? 2.0 : 1.0;
+   }
+
+   @Override
+   public double getDoubleSupportDuration()
+   {
+      return runningOnRealRobot ? 1.5 : 0.25;
+   }
 
    @Override
    public double getAdditionalTimeForSingleSupport()
@@ -29,42 +29,42 @@ public class IcubCapturePointPlannerParameters implements CapturePointPlannerPar
       return 0.1;
    }
 
-	@Override
-	public double getSingleSupportDuration()
-	{
-		return runningOnRealRobot ? 1.5 : 0.7;
-	}
+   @Override
+   public double getSingleSupportDuration()
+   {
+      return runningOnRealRobot ? 1.5 : 0.7;
+   }
 
-	@Override
-	public int getNumberOfFootstepsToConsider()
-	{
-		return 3;
-	}
+   @Override
+   public int getNumberOfFootstepsToConsider()
+   {
+      return 3;
+   }
 
-	@Override
-	public int getNumberOfCoefficientsForDoubleSupportPolynomialTrajectory()
-	{
-		return 5;
-	}
+   @Override
+   public int getNumberOfCoefficientsForDoubleSupportPolynomialTrajectory()
+   {
+      return 5;
+   }
 
-	@Override
-	public int getNumberOfFootstepsToStop()
-	{
-		return 2;
-	}
+   @Override
+   public int getNumberOfFootstepsToStop()
+   {
+      return 2;
+   }
 
-	@Override
-	public double getIsDoneTimeThreshold()
-	{
-		return -1e-4;
-	}
+   @Override
+   public double getIsDoneTimeThreshold()
+   {
+      return -1e-4;
+   }
 
    @Override
    public double getDoubleSupportSplitFraction()
    {
       return 0.5;
    }
-   
+
    @Override
    public double getFreezeTimeFactor()
    {
@@ -76,7 +76,7 @@ public class IcubCapturePointPlannerParameters implements CapturePointPlannerPar
    {
       return 0.02;
    }
-   
+
    @Override
    public boolean getDoTimeFreezing()
    {
@@ -95,23 +95,23 @@ public class IcubCapturePointPlannerParameters implements CapturePointPlannerPar
       return 0.65;
    }
 
-	@Override
-	public double getCapturePointInFromFootCenterDistance() 
-	{
-		return 0.006;
-	}
+   @Override
+   public double getReferenceCMPInsideOffset()
+   {
+      return 0.006;
+   }
 
-	@Override
-	public double getCapturePointForwardFromFootCenterDistance() 
-	{
-		return 0.0;
-	}
-	
-	@Override
-	public double getMaxAllowedErrorWithoutPartialTimeFreeze()
-	{
-		return 0.03;
-	}
+   @Override
+   public double getReferenceCMPForwardOffset()
+   {
+      return 0.0;
+   }
+
+   @Override
+   public double getMaxAllowedErrorWithoutPartialTimeFreeze()
+   {
+      return 0.03;
+   }
 
    @Override
    public boolean useTerribleHackToReduceICPVelocityAtTheEndOfTransfer()
@@ -119,4 +119,39 @@ public class IcubCapturePointPlannerParameters implements CapturePointPlannerPar
       return true;
    }
 
+   @Override
+   public boolean useNewICPPlanner()
+   {
+      return false;
+   }
+
+   @Override
+   public boolean useTwoCMPsPerSupport()
+   {
+      return false;
+   }
+
+   @Override
+   public double getTimeSpentOnExitCMPInPercentOfStepTime()
+   {
+      return 0.50;
+   }
+
+   @Override
+   public double getMaxReferenceCMPForwardOffset()
+   {
+      return 0.05;
+   }
+
+   @Override
+   public double getMinReferenceCMPForwardOffset()
+   {
+      return -0.02;
+   }
+
+   @Override
+   public double getSafeDistanceForSupportEdges()
+   {
+      return 0.03;
+   }
 }
