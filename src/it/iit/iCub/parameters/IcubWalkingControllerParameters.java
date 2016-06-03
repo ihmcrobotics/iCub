f64bbeecb2e2824dd264f9b6122af1f25d9d12f7
Robert <rgriffin@ihmc.us>
2016-06-03 12:41:55 -0500

    fixed the walking controller parameters for the R2 platform

--- src/it/iit/iCub/parameters/IcubWalkingControllerParameters.java
+++ src/it/iit/iCub/parameters/IcubWalkingControllerParameters.java
@@ -898,4 +898,16 @@ public class IcubWalkingControllerParameters extends WalkingControllerParameters
    {
       return Double.POSITIVE_INFINITY;
    }
+
+   @Override
+   public void useInverseDynamicsControlCore()
+   {
+      // once another mode is implemented, use this to change the default gains for inverse dynamics
+   }
+
+   @Override
+   public void useVirtualModelControlCore()
+   {
+      // once another mode is implemented, use this to change the default gains for virtual model control
+   }
 }
