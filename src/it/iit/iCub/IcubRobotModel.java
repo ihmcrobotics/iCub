7467cfc72db73ad53c7ab3203c1be11accc5c3b5
Nathan Mertins <nmertins@ihmc.us>
2016-10-10 13:38:01 -0500

    removed SDF prefix from JointNameMaps

--- src/it/iit/iCub/IcubRobotModel.java
+++ src/it/iit/iCub/IcubRobotModel.java
@@ -23,7 +23,7 @@ import us.ihmc.SdfLoader.JaxbSDFLoader;
 import us.ihmc.SdfLoader.RobotDescriptionFromSDFLoader;
 import us.ihmc.SdfLoader.models.FullHumanoidRobotModel;
 import us.ihmc.humanoidRobotics.HumanoidFloatingRootJointRobot;
-import us.ihmc.SdfLoader.SDFHumanoidJointNameMap;
+import us.ihmc.SdfLoader.HumanoidJointNameMap;
 import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
 import us.ihmc.SdfLoader.models.FullRobotModel;
 import us.ihmc.robotics.partNames.NeckJointName;
@@ -312,7 +312,7 @@ public class IcubRobotModel implements DRCRobotModel
    {
       boolean useCollisionMeshes = false;
       boolean enableTorqueVelocityLimits = false;
-      SDFHumanoidJointNameMap jointMap = getJointMap();
+      HumanoidJointNameMap jointMap = getJointMap();
       boolean enableJointDamping = getEnableJointDamping();
       return loader.createRobot(jointMap.getModelName(), jointMap, useCollisionMeshes, enableTorqueVelocityLimits, enableJointDamping);
    }
