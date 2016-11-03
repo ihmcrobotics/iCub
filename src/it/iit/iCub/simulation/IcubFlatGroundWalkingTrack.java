e33db5f8def1d063c6c4f117a3c42a522178ffa2
Duncan Calvert <dcalvert@ihmc.us>
2016-11-03 17:59:28 -0500

    NRI-153: Move OscillateFeetPerturber controller doen to common walking. Had to move floating humanoid to SCS out of newly created simulation toolkit.

--- src/it/iit/iCub/simulation/IcubFlatGroundWalkingTrack.java
+++ src/it/iit/iCub/simulation/IcubFlatGroundWalkingTrack.java
@@ -1,13 +1,13 @@
 package it.iit.iCub.simulation;
 
 import it.iit.iCub.IcubRobotModel;
-import us.ihmc.humanoidRobotics.HumanoidFloatingRootJointRobot;
 import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCGuiInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCSCSInitialSetup;
 import us.ihmc.graphics3DAdapter.GroundProfile3D;
+import us.ihmc.simulationconstructionset.HumanoidFloatingRootJointRobot;
 import us.ihmc.simulationconstructionset.util.ground.FlatGroundProfile;
 
 public class IcubFlatGroundWalkingTrack
