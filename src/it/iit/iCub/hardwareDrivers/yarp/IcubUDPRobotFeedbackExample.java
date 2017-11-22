d17c7734ea12b79489657cc5755c0e753c8cc431
Doug Stephen <dstephen@ihmc.us>
2017-11-22 11:44:22 -0600

    Simple feedback example is now working.

--- src/it/iit/iCub/hardwareDrivers/yarp/IcubUDPRobotFeedbackExample.java
+++ src/it/iit/iCub/hardwareDrivers/yarp/IcubUDPRobotFeedbackExample.java
@@ -1,8 +1,62 @@
 package it.iit.iCub.hardwareDrivers.yarp;
 
+import it.iit.iCub.messages.it.iit.yarp.RobotFeedback;
+import us.ihmc.commons.Conversions;
+import us.ihmc.util.PeriodicNonRealtimeThreadScheduler;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
  */
-public class IcubUDPRobotFeedbackExample
+public class IcubUDPRobotFeedbackExample implements Runnable
 {
+   private final RobotFeedback iCubRobotFeedback = new RobotFeedback();
+   private final PeriodicNonRealtimeThreadScheduler scheduler = new PeriodicNonRealtimeThreadScheduler(getClass().getSimpleName());
+   private final IcubUDPRobotFeedbackReceiver feedbackReceiver = new IcubUDPRobotFeedbackReceiver("192.168.96.1",
+                                                                                                  IcubUDPRobotFeedbackReceiver.YARP_ROBOT_FEEDBACK_PORT,
+                                                                                                  iCubRobotFeedback);
+
+   private void start() throws IOException, InterruptedException
+   {
+      feedbackReceiver.connect();
+      scheduler.schedule(this, Conversions.secondsToNanoseconds(0.005), TimeUnit.NANOSECONDS);
+
+      //      Thread thread = new Thread(this);
+      //      thread.start();
+      //      thread.join();
+   }
+
+   @Override
+   public void run()
+   {
+      //      while(true)
+      //      {
+      try
+      {
+         long receive = feedbackReceiver.receive();
+
+         System.out.println(iCubRobotFeedback);
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+      //      }
+   }
+
+   public static void main(String[] args)
+   {
+      IcubUDPRobotFeedbackExample icubUDPRobotFeedbackExample = new IcubUDPRobotFeedbackExample();
+
+      try
+      {
+         icubUDPRobotFeedbackExample.start();
+      }
+      catch (IOException | InterruptedException e)
+      {
+         e.printStackTrace();
+      }
+   }
 }
---
+++ src/it/iit/iCub/hardwareDrivers/yarp/IcubUDPRobotFeedbackReceiver.java
@@ -0,0 +1,99 @@
+package it.iit.iCub.hardwareDrivers.yarp;
+
+import it.iit.iCub.messages.it.iit.yarp.RobotFeedback;
+import it.iit.iCub.messages.it.iit.yarp.RobotFeedbackPubSubType;
+import us.ihmc.idl.CDR;
+import us.ihmc.pubsub.common.SerializedPayload;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.StandardProtocolFamily;
+import java.net.StandardSocketOptions;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+/**
+ * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
+ */
+public class IcubUDPRobotFeedbackReceiver
+{
+   public static final int YARP_ROBOT_FEEDBACK_PORT = 9970;
+   private final RobotFeedbackPubSubType robotFeedbackPubSubType = new RobotFeedbackPubSubType();
+   private final SerializedPayload payload = new SerializedPayload(robotFeedbackPubSubType.getTypeSize());
+   private final String yarpHostOrIP;
+   private final int yarpUDPPort;
+   private final RobotFeedback iCubRobotFeedbackToUpdate;
+
+   private DatagramChannel receiveChannel = null;
+
+   public IcubUDPRobotFeedbackReceiver(String yarpHostOrIP, int yarpUDPPort, RobotFeedback iCubRobotFeedbackToUpdate)
+   {
+      this.yarpHostOrIP = yarpHostOrIP;
+      this.yarpUDPPort = yarpUDPPort;
+      this.iCubRobotFeedbackToUpdate = iCubRobotFeedbackToUpdate;
+   }
+
+   public synchronized long receive() throws IOException
+   {
+      ByteBuffer payloadData = payload.getData();
+
+      payloadData.clear();
+
+      CDR.writeEncapsulation(payload);
+
+      try
+      {
+         System.out.println("Waiting to receive...");
+         receiveChannel.receive(payloadData);
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
+
+      payloadData.flip();
+
+      long currentTime = System.nanoTime();
+      robotFeedbackPubSubType.deserialize(payload, iCubRobotFeedbackToUpdate);
+      return currentTime;
+   }
+
+   public void connect() throws IOException
+   {
+      try
+      {
+         receiveChannel = DatagramChannel.open(StandardProtocolFamily.INET).setOption(StandardSocketOptions.SO_REUSEADDR, true)
+                                        .bind(new InetSocketAddress(yarpHostOrIP, yarpUDPPort));
+         receiveChannel.socket().setReceiveBufferSize(65535);
+         receiveChannel.socket().setSoTimeout(1000);
+
+//         System.out.println("Local address: " + receiveChannel.getLocalAddress());
+//         System.out.println("Remote address: " + receiveChannel.getRemoteAddress());
+//         System.out.println("Is connected: " + receiveChannel.isConnected());
+//         System.out.println("Is blocking: " + receiveChannel.isBlocking());
+      }
+      catch (IOException e)
+      {
+         receiveChannel.disconnect();
+         receiveChannel.close();
+         receiveChannel = null;
+      }
+   }
+
+   public void disconnect()
+   {
+      if (receiveChannel != null)
+      {
+         try
+         {
+            receiveChannel.disconnect();
+            receiveChannel.close();
+            receiveChannel = null;
+         }
+         catch (IOException e)
+         {
+            e.printStackTrace();
+         }
+      }
+   }
+}
